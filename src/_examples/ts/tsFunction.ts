a4fbb35d2d362c701b0a16375bc17c4d2a29877a
rrain <rrainpath@gmail.com>
2025-01-11 14:47:12 +0800

    refactor

--- src/_examples/ts/tsFunction.ts
+++ src/_examples/ts/tsFunction.ts
@@ -3,6 +3,10 @@
 {
   // Function type - variant 1
   type UserGenerator = (username: string, email?: string) => string;
+  const userGenerator: UserGenerator = (username: string, email?: string) => ''
+  {
+    const userGenerator: UserGenerator = function(username: string, email?: string) { return '' }
+  }
 }
 {
   // Function type overloading - variant 1
@@ -10,16 +14,22 @@
     (username: string): string;
     (username: string, email: string): string;
   };
+  const userGenerator: UserGenerator = (username: string, email?: string) => ''
+  {
+    const userGenerator: UserGenerator = function(username: string, email?: string) { return '' }
+  }
 }
 
 
 {
   // Function type - variant 2
+  // Can't use arrow function syntax
   function userGenerator(username: string, email?: string): string
   function userGenerator(username: string, email?: string) { return '' }
 }
 {
   // Function type overloading - variant 2
+  // Can't use arrow function syntax
   function userGenerator(username: string): string
   function userGenerator(username: string, email: string): string
   function userGenerator(username: string, email?: string) { return '' }
